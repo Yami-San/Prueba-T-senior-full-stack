5aaf1d663bbd32732d694b83f94a8b3f7f467fb9
Jean Marco Pedraza Z <jeanmarcopedraza@gmail.com>
2025-02-27 16:49:06 -0500

    transactions page almost done

--- pages/transactions-table.tsx
+++ pages/transactions-table.tsx
@@ -6,12 +6,16 @@ export default function Userstransactions() {
   
     return (
     <Layout>
-      <div className="max-w-3xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
+      <div className="w-2/4 mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
         <h1 className="text-xl font-bold text-center mb-4">
           Sistema de gestión de Ingresos y Gastos
         </h1>
-        <h2 className="text-lg font-semibold border-b mb-2 text-center">Gestión de suarios</h2>
-        <Button className="mb-4 px-4 py-2 bg-gray-600 rounded hover:bg-gray-700 hover:scale-110 ">Nuevo</Button>
+        <h2 className="text-lg font-semibold border-b mb-2 text-center">Movimientos</h2>
+        <div className="flex justify-end mb-4">
+          <Button className="px-4 py-2 bg-gray-600 rounded hover:bg-gray-700 hover:scale-110">
+            Nueva transacción
+          </Button>
+      </div>
         <TransactionsTable></TransactionsTable>
         </div>
       </Layout>
