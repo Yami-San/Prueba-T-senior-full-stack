35e2cb0f5f5cfcaa03b2007871f0fffdff92ca9c
Jean Marco Pedraza Z <jeanmarcopedraza@gmail.com>
2025-02-27 14:39:29 -0500

    next auth changes

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,13 +2,21 @@ import { DefaultSession, DefaultUser } from "next-auth"
 import { Role } from "@prisma/client"
 
 declare module "next-auth" {
+  // Extiende la interfaz User
   interface User extends DefaultUser {
+    // Si quieres que 'id' y 'role' sean obligatorios, quita '?'
+    id: string
     role?: Role
   }
 
+  // Extiende la interfaz Session
   interface Session {
-    user?: {
+    user: {
+      // Si no quieres id opcional, quita '?'
+      id: string
+      // Role opcional o no, depende de tu lógica
       role?: Role
     } & DefaultSession["user"]
   }
 }
+
